8cb6ae81a14fec0af3bdede24b18cb791a938c6a
Faisal Ahammad <faisalahammad24@gmail.com>
2018-12-22 18:38:33 +0600

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# js-loan-calculator
-Javascript Loan Calculator
+# Javascript Loan Calculator
+
+Javascript Loan Calculator using raw Javascript & DOM.
+
+### Features
+
+- Mobile responsive design
+- Input custom loan amount
+- Input custom interest percentage
+- Input custom loan return year duration
+- Empty input checking and warning
+- jQuery v3.3.1 support
+- Twitter Bootstrap v4.2.1 support
+
+### Screenshot
+
+<img src="img/screenshot-1.jpg" alt="Screenshot 1">
+<em>Loan Calculate Homepage</em><br><br>
+
+<img src="img/screenshot-2.jpg" alt="Screenshot 2">
+<em>Loan Calculate result</em>
---
+++ scripts.js
@@ -0,0 +1,69 @@
+// Listen for Submit
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    // Hide Results
+    document.getElementById('results').style.display = 'none';
+    // Show Loader
+    document.getElementById('loading').style.display = 'block';
+
+    // Show result after 2s
+    setTimeout(calculateResults, 1000);
+
+    e.preventDefault();
+});
+
+// Calculate
+function calculateResults() {
+
+    // UI Variable
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    // Compute Monthly Payment
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if (isFinite(monthly)) {
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        // Show Results
+        document.getElementById('results').style.display = 'block';
+        // Hide Loader
+        document.getElementById('loading').style.display = 'none';
+    } else {
+        showError('Please check your number');
+    }
+
+    // Show Error
+    function showError(error) {
+        // Hide Loader
+        document.getElementById('loading').style.display = 'none';
+        // Create a div
+        const errorDiv = document.createElement('div');
+        errorDiv.className = 'alert alert-danger';
+
+        // Get Element
+        const card = document.querySelector('.card');
+        const heading = document.querySelector('.heading');
+
+        // Create text node and append to div
+        errorDiv.appendChild(document.createTextNode(error));
+
+        // Insert error before card
+        card.insertBefore(errorDiv, heading);
+
+        // Clear error after 3s
+        setTimeout(function () {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+}
